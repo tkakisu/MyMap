dd7ec84b2f83ce3c5229ce985ee638e3ad692207
Takahiro Kakisu <takahirokakisu@gmail.com>
2025-08-16 16:57:11 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by takahiro kakisu on 2025/08/16.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
