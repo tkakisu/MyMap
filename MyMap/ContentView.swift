cf2e4ffeb67de1353346d88d69cecd1fdc8e7704
Takahiro Kakisu <takahirokakisu@gmail.com>
2025-08-16 19:11:12 +0900

    キーワード検索機能を実装

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 入力中の文字列を保持する状態変数
+    @State var inputText: String = ""
+    // 検索キーワードを保持する状態変数、初期値は"東京駅"
+    @State var displaySearchKey: String = "東京駅"
+    
     var body: some View {
         VStack {
-            VStack {
-                // マップを表示
-                MapView(searchKey: "羽田空港")
-            }
+            // テキストフィールド
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+                .onSubmit {
+                    // 入力が完了したので検索キーワードに設定する
+                    displaySearchKey = inputText
+                }
+                .padding()
+            
+            // マップを表示
+            MapView(searchKey: displaySearchKey)
         }
         .padding()
     }
